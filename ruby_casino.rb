6512df9a57b7b5b6e4d9084acfcdc6ddcfb82f7e
Pat <patrick.seiuli@gmail.com>
2017-08-21 10:28:35 -0600

    initial high low and menu

--- ruby_casino.rb
+++ ruby_casino.rb
@@ -1,5 +1,6 @@
 require "pry"
 require_relative "player"
+require_relative "high_low"
 
 class Casino
   attr_accessor :player
@@ -7,7 +8,24 @@ class Casino
   def initialize
     puts "Welcome To The Ruby Casino"
     @player = Player.new
+    menu
   end
+
+  def menu
+    puts "1) Play HighLow"
+    puts "2) Quit"
+    case gets.to_i
+      when 1
+        HighLow.new(player)
+      when 2
+        puts 'Thanks for playing!'
+        exit
+      else
+        puts 'Invalid Choice! Try Again!'
+        menu
+      end
+      menu
+    end
 end
 
 Casino.new
